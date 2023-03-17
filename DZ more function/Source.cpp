43b57988e6633617506e5ff23ba61681196c37be
Дмитрий <@ADMIN>
2023-03-17 05:12:18 +0500

    12

--- DZ more function/Source.cpp
+++ DZ more function/Source.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <algorithm>
+#include <ctime>
+#include <cstdlib>
 /*Task 1.*/
 int func(int n, int m) {
 	for (int i = n / 2; i >= 1; i--) {
@@ -28,26 +30,40 @@ void mass(T arr[], const int length) {
 	std::cout << "\b\b]\n";
 }
 //Array worker function
-int move_mass(int arr[], const int length, int n) {
+void move_mass(int arr[], const int length, int n) {
 	int b = 0;
-	for (int i = 0; i < length; i++)
+	for (int i = 0; i < length; i++) 
 		if (arr[i] == n) {
-			return i;
-		}
-	for(int i = length - 1; i > 0; i--)
-		if (arr[i] < 0) {
 			b = i;
 			break;
 		}
-	// sort
-	for (int i = b - 1; i > n + 1; i--) {
-		for (int j = n; i < j; j++)
-			if (arr[j] > arr[i + 1])
-				std::swap(arr[j], arr[j + 1]);
-	}
-	return -1;
+		// sort		
+		std::sort(arr + b + 1, arr + length);		
+		std::cout << "\nИндекс заданного числа:  "<< arr[b] <<" является: " << b <<'\n'<< std::endl;
 }
 
+//Task 4.
+void fill_arr(int arr[], const int length, int num1, int num2) {
+	int start = 0;
+	srand(time(NULL));
+	for (int i = 0; i < length; i++) 
+		arr[i] = rand()%(num1 - num2) + num1;
+}
+void sort_mass(int arr[], const int length) {
+	int first = 0, last = 0;
+	for (int i = 0; i < length; i++)
+		if (arr[i] < 0) {
+			first = i;
+			break;
+		}
+	for (int i = length - 1; i > 0; i--)
+		if (arr[i] < 0) {
+			last = i;
+			break;
+		}
+	for (int i = first; i < last; i++)
+		arr[i];
+}
 
 int main() {
 	setlocale(LC_ALL, "Rus");
@@ -71,15 +87,34 @@ int main() {
 		/*Task 3.
 			The function takes an array, its lengthand, and the number N. We find the element of the array,
 			sort the number to the right of the given number and display its position.*/
-
+	/*std::cout << "\nTask 3.\nSort the number to the right of the given number and display its position.\n";
 	const int size = 10;
 	int array[size]{ 7, 6, 10, 45, 56, 67, 78, 4, 12, 66 };
 	int f = 45;
+	std::cout << "\nИзначальный массив: \n";
 	mass(array, size);
 	
-	std::cout << move_mass(array, size, f) << std::endl;
+	 move_mass(array, size, f);
+	 std::cout << "Итоговый массив: \n";
+	 mass(array, size);
+	 std::cout << std::endl;
+	 */
 
-
+	/*Task 4
+		Create a function that accepts empty
+	array, its length and two numbers A and B which are
+	start and end of the range. The function should
+	fill an array with random numbers from the specified
+	range.*/
+	std::cout << "\nTask 4.\nThe function an array with random numbers from the specified range.\n";
+	std::cout << "Initial array : \n";
+	const int size1 = 15;
+	int arr1[size1];
+	fill_arr(arr1, size1, 10, 15);
+	mass(arr1, size1);
+	sort_mass(arr1, size1);
+	mass(arr1, size1);
+	std::cout << std::endl;
 
 	return 0;
 }
